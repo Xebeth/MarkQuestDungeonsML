25ceb8cb23cec7ccdb29c9bf572ff21f0a4d2e4c
Xebeth <github@north-edge.com>
2025-05-25 01:33:28 +0100

    Update the project to use MagicLoaderGenerator 1.2.0

--- MarkQuestDungeons/Program.cs
+++ MarkQuestDungeons/Program.cs
@@ -1,14 +1,29 @@
-﻿using MagicLoaderGenerator.Localization.Providers;
+﻿using MagicLoaderGenerator.Filesystem.Abstractions;
+using MagicLoaderGenerator.Localization.Transforms;
+using MagicLoaderGenerator.Localization.Providers;
 using MagicLoaderGenerator.Filesystem.Generators;
 using Microsoft.Extensions.Configuration;
+using MagicLoaderGenerator;
 using MarkQuestDungeonsML;
 
 var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
                                         .AddJsonFile("config.json", optional: false);
+var variants = new Dictionary<string, IMagicLoaderFileTransform?>();
+var appConfig = new MarkQuestDungeonsConfig(builder.Build());
+var localization = new JsonLocalizationProvider(appConfig);
+var mod = new MagicLoaderMod(localization, appConfig);
 
-IConfiguration config = builder.Build();
+foreach (var (variant, prefix) in appConfig.Suffixes)
+{
+    variants.Add(variant, new ValueSuffixTransformer(localization, prefix));
+}
 
-var appConfig = new MarkQuestDungeonsConfig(config);
-var mod = new MagicLoaderMod(new JsonLocalizationProvider(appConfig), appConfig);
+var outputDir = mod.Generate(new ZipOutputGenerator(appConfig), variants);
 
-mod.Generate(new ZipOutputGenerator(appConfig), true);
+#if DEBUG
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
+    UseShellExecute = true,
+    FileName = outputDir,
+    Verb = "open"
+});
+#endif
